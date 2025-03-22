3c55d0eb502e1c5cda03689cb79c40498ac2c6e8
Haruna Afolabi <aaronicks@gmail.com>
2025-03-22 20:10:00 +0100

    Working on footer

--- babaoja/static/js/custom.js
+++ babaoja/static/js/custom.js
@@ -1,6 +1,40 @@
 // Flipping the card
-document.querySelectorAll('.flip-btn').forEach(button => {
-    button.addEventListener('click', function() {
-        this.closest('.flip-card').classList.toggle('flipped');
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".flip-card").forEach(function (card) {
+        const viewBtn = card.querySelector(".view-btn");
+        const backBtn = card.querySelector(".back-btn");
+
+        viewBtn.addEventListener("click", function () {
+            card.classList.add("flipped");
+        });
+
+        backBtn.addEventListener("click", function () {
+            card.classList.remove("flipped");
+        });
     });
 });
+
+
+
+
+
+// hamburger js
+document.addEventListener("DOMContentLoaded", function() {
+    const toggler = document.querySelector(".custom-toggler");
+
+    toggler.addEventListener("click", function() {
+        this.classList.toggle("open"); // Toggle 'X' animation
+    });
+});
+
+
+// side navbar
+function toggleSidebar() {
+    let sidebar = document.getElementById("mySidebar");
+    let toggleBtn = document.getElementById("sidebarToggle");
+
+    sidebar.classList.toggle("active");
+    toggleBtn.classList.toggle("hidden"); // Hide button when sidebar is active
+}
+
+
